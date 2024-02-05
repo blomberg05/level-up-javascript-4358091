a146de77802575e4210be3046c34847e484a7a30
Fredrik <blomberg05@gmail.com>
2024-02-05 08:24:28 +0000

    Try first commit

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,31 @@
-// Write your code here
+class Books {
+
+  constructor(title, author, isbn, numOfCopies) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.numOfCopies = numOfCopies;
+  }
+
+  get Availibility() {
+    switch(this.numOfCopies){
+      case  this.numOfCopies === 0 : {
+        return "Out of stock"
+      }
+      case this.numOfCopies < 10 : {
+        return "Low stock"
+      }
+      default : {
+       return  "In stock"
+      }
+    }
+  }
+  sell(numOfCopiesSold = 1) {
+    return this.numOfCopies -= numOfCopiesSold;
+  }
+
+  restock(copiesRestock = 5) {
+    return this.numOfCopies += copiesRestock;
+  }
+
+}
