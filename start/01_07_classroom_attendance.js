a4f36604b7974687e53bfd2badd7015476744c46
Fredrik <blomberg05@gmail.com>
2024-02-09 12:43:45 +0000

    Fixed task

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,14 @@
-// Write your code here
+const classrom = ["Rashida","John", "Susanna", "Mikael", "Sara"];
+
+
+function getStudents(hasAssistent = false) {
+  if(hasAssistent) {
+    const [teacher, assistent, ...students] = classrom;
+    return `Techer: ${teacher} assistent: ${assistent} students: ${students}`
+  } else {
+    const [teacher, ...students] = classrom;
+        return `Techer: ${teacher} students: ${students}`
+
+  }
+}
+  console.log( getStudents(true));
