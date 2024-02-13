23b29d35683374309518eb5c135afab9a65e5a94
Fredrik <blomberg05@gmail.com>
2024-02-13 13:35:52 +0000

    Fixed some task

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,14 @@
 // Write your code here
+var order = [2,3,1,5];
+var coffePrice = 1.25;
+
+var sum = order.reduce((previous,current) => {
+  return previous + current;
+},0)
+
+var totalBill = coffePrice * sum;
+
+console.log(`Total price ${totalBill}`);
+
+
+
